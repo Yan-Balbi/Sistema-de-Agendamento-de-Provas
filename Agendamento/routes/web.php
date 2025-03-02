6a9f3ae0f9833ae01e47e6f4cf111d82556624aa
Yan-Balbi <yanbalbi@gmail.com>
2025-03-02 14:05:34 -0300

    Melhorando a navegação.

--- Agendamento/routes/web.php
+++ Agendamento/routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\SalaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function(){
     return view('header');
 });
 
