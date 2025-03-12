ae9ed2266848e6ad2d0f491f4201ed12b4b759d1
EdivanAzevedo <66445757+EdivanAzevedo@users.noreply.github.com>
2025-03-12 10:18:02 -0300

    Corrigido bug de usuário não logado acessar rotas
    Os cadastras estavam publicos, foi protegido estas rotas para somente usuário logados possam acessar elas.

--- Agendamento/routes/web.php
+++ Agendamento/routes/web.php
@@ -13,23 +13,19 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::prefix('/')->group(base_path('routes/professores.php'));
-Route::prefix('/')->group(base_path('routes/salas.php'));
-Route::prefix('/')->group(base_path('routes/agendamentos.php'));
-
-Route::prefix('/roles')->group(base_path('routes/roles.php'));
-Route::prefix('/users')->group(base_path('routes/users.php'));
-
-Route::resource('cursos', CursoController::class);
-
-Route::resource('turmas', TurmaController::class);
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', function () { return view('dashboard');})->name('dashboard');
+    Route::resource('cursos', CursoController::class);
+    Route::prefix('/')->group(base_path('routes/salas.php'));
+    Route::resource('turmas', TurmaController::class);
+    Route::resource('cursos', CursoController::class);
+    Route::prefix('/users')->group(base_path('routes/users.php'));
+    Route::prefix('/roles')->group(base_path('routes/roles.php'));
+    Route::prefix('/')->group(base_path('routes/agendamentos.php'));
+    Route::prefix('/')->group(base_path('routes/salas.php'));
+    Route::prefix('/')->group(base_path('routes/professores.php'));
 });
